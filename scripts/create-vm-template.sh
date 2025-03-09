e13c3074f60bee92659dea8a8597f4c08f1acee9
brad-j <brad@bradjohnson.io>
2025-03-09 13:38:36 -0400

    Script lookin tight looking right

--- scripts/create-vm-template.sh
+++ scripts/create-vm-template.sh
@@ -1,60 +1,50 @@
 #!/usr/bin/env bash
 
-# Colors
-BOLD_CYAN="\e[1;36m"
-NC='\033[0m'
-
 clear
-printf "Enter url for cloud image iso: \n" iso_url
-read iso_url 
+printf "Enter url for cloud image iso: \n" ISO_URL
+read ISO_URL
 
-# printf "${BOLD_CYAN}Downloading${NC} $iso_url \n\n"
+printf "Downloading: $ISO_URL \n\n"
 
-# wget "$iso_url"
+wget "$ISO_URL"
 
-printf "Next we need to resize the disk. Enter the size of the disk in gigabytes. (Default is 32 GB) \n" disk_size
-read disk_size
+template_name_from_url=$(basename "$ISO_URL")
 
-printf "Enter template name. This is the name shown in Proxmox next to the VM id. (Default is the image name) \n" template_name
-read template_name
+printf "Next we need to resize the disk. Enter the size of the disk in gigabytes. Example: 256G. (Default is 32G) \n" DISK_SIZE
+read DISK_SIZE
 
-template_name_from_url=$(basename "$iso_url")
+disk_size=${DISK_SIZE:="32G"}
+
+qemu-img resize $template_name_from_url $disk_size
+
+printf "Enter template name. This is the name shown in Proxmox next to the VM id. (Default is the image name) \n" TEMPLATE_NAME
+read TEMPLATE_NAME
+
 template_name_no_extension="${template_name_from_url%.*}"
 
-# printf "${BOLD_CYAN}Image name:${NC} %s\n" "$template_name_no_extension"
+printf "Enter VM id (number) \n" VMID
+read VMID
 
-printf "Enter VM id (number) \n" vmid
-read vmid
+printf "How much memory for the VM in Megabytes? (Default is 1024 MB) \n" MEMORY
+read MEMORY
 
-# printf "VM id: $vmid"
+printf "How many CPU cores for the VM? (Default is 1 core) \n" CORES
+read CORES
 
-printf "How much memory for the VM in Megabytes? (Default is 1024 MB) \n" memory
-read memory
-
-printf "How many CPU cores for the VM? (Default is 1 core) \n" cores
-read cores
-
-printf "Which network bridge should it use? (Default is vmbr0) \n" bridge
-read bridge
+printf "Which network bridge should it use? (Default is vmbr0) \n" BRIDGE
+read BRIDGE
 
 printf "\n"
 
 printf "If all of the choices look correct, press enter/return to create the VM. (CTRL + C to start over) \n" confirmation
 
-# Default values
-default_template_name=${template_name_no_extension}
-default_vmid=5001
-default_memory=1024
-default_cores=1
-default_bridge="vmbr0"
+name=${TEMPLATE_NAME:=template_name_no_extension}
+vmid=${VMID:="5001"}
+memory=${MEMORY:="1024"}
+cores=${CORES:="1"}
+bridge=${BRIDGE:="vmbr0"}
 
-name="${1:-$default_template_name}"
-vmid="${2:-$default_vmid}"
-memory="${3:-$default_memory}"
-cores="${4:-$default_cores}"
-bridge="${5:-$default_bridge}"
-
-printf "%s\n" "Disk Size: ${disk_size}" "Template Name: ${template_name}" "VM id: ${vmid}" "Memory: ${memory}" "CPU Cores: ${cores}" "Network Bridge: ${bridge}"
+printf "%s\n" "Template Name: ${name}" "VM id: ${vmid}" "Memory: ${memory}" "CPU Cores: ${cores}" "Network Bridge: ${bridge}"
 
 qm create $vmid --name "$name" --ostype l26 \
     --memory "$memory" \
